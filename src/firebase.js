36c6b6728f58db61eb7b27231346199c725114e9
Simion Robert <robertcosmin.simion@bcr.ro>
2022-03-22 09:48:55 +0200

    Console log

--- src/firebase.js
+++ src/firebase.js
@@ -17,8 +17,8 @@ import {
   addDoc,
 } from "firebase/firestore";
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 console.log(process.env);
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
